12d9da58918cb36e2419fb132bc094472b7b1879
Pykir <97310051+Pykir@users.noreply.github.com>
2026-01-31 11:33:09 -0500

    Update player.gd
    Replaced default input mapping with custom and added some comments

--- player.gd
+++ player.gd
@@ -7,18 +7,21 @@ const ACC = 50.0
 
 var direction = Vector2.ZERO
 var target_velocity = Vector2.ZERO
+
 func _physics_process(delta: float) -> void:
 	
-	if Input.is_action_pressed("ui_right"):
+	#movement input
+	if Input.is_action_pressed("move_right"):
 		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_down"):
 		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
 	target_velocity = (direction.normalized() * SPEED)
 	
+	#acceleration & friction calculations
 	if direction != Vector2.ZERO:
 		velocity.x = move_toward(velocity.x, target_velocity.x, ACC * delta)
 		velocity.y = move_toward(velocity.y, target_velocity.y, ACC * delta)
@@ -27,5 +30,6 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y, 0, FRICTION * delta)
 	velocity = velocity.clampf(-SPEED,SPEED)
 
+	#move and reset direction for next physics tick
 	move_and_slide()
 	direction = Vector2.ZERO
