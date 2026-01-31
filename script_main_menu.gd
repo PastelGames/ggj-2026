21a8747c69b2a74ac180acb0cbd1ac5a84ba6b6b
lxklp <96218125+lxklp@users.noreply.github.com>
2026-01-31 16:08:53 -0500

    Add main menu and setting (#5)
    * Add Dialogue Speaking Screen
    
    * Add Dialogue Choosing Screen
    
    * Turn the display and choosing panels into scenes
    
    * Add scenes: main menu and setting
    
    * Fix spelling error
    
    ---------
    
    Co-authored-by: El Makes Games 3 <43424070+PastelGames@users.noreply.github.com>

---
+++ script_main_menu.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_button_pressed() -> void:
+	pass
+
+
+func _on_setting_button_pressed() -> void:
+	get_node("Setting").show()
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
