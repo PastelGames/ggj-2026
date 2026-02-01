33d194b26d0117a7ad8c8fd07243c47dd680ab6c
El Makes Games 3 <43424070+PastelGames@users.noreply.github.com>
2026-02-01 17:01:31 -0500

    Art assets to dialogue (#24)
    * Functional Dialogue System
    
    * Working Dialogue with art

--- Scripts/DialogueManager.gd
+++ Scripts/DialogueManager.gd
@@ -2,28 +2,32 @@ class_name DialogueManager
 extends StateMachine
 
 @onready var dialogue_panel: Panel = $DialoguePanel
-@onready var response_panel: Panel = $ResponsePanel
+@onready var response_panel: Control = $ResponsePanel
 
 @onready var dialogue_label: Label = $DialoguePanel/DialogueLabel
+@onready var speaker_thumbnail_texture: TextureRect = $DialoguePanel/SpeakerThumbnailTexture
 @onready var left_option_button: Button = $ResponsePanel/LeftOptionButton
 @onready var right_option_button: Button = $ResponsePanel/RightOptionButton
 
 var _current_dialogue_index: int = 0
 
 @export var _dialogue_interaction_data : DialogueInteractionData
+@export var _speaker_icon_dictionary : Dictionary[String, Texture]
 
 var dialogue_interaction_data: DialogueInteractionData:
 	get:
 		return _dialogue_interaction_data
 	set(val):
 		_dialogue_interaction_data = val
+		_current_dialogue_index = 0
+		if not dialogue_interaction_data.music.is_empty():
+			MusicManager.set_bgm_and_play(dialogue_interaction_data.music)
 		begin_dialogue_interaction()
 
 
 func _ready() -> void:
 	super._ready()
-	begin_dialogue_interaction()
-
+	
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
@@ -41,12 +45,19 @@ func _advance_dialogue_interaction():
 
 func begin_dialogue_interaction():
 	_current_dialogue_index = 0
-	display_dialogue(_dialogue_interaction_data.dialogue[_current_dialogue_index])
+	display_dialogue(dialogue_interaction_data.dialogue[_current_dialogue_index].text)
 	transition_to_state("DialogueState")
 
 
 func display_dialogue_at_current_index():
-		display_dialogue(dialogue_interaction_data.dialogue[_current_dialogue_index])
+		display_dialogue(dialogue_interaction_data.dialogue[_current_dialogue_index].text)
+		var new_texture = _speaker_icon_dictionary[dialogue_interaction_data.dialogue[_current_dialogue_index].speaker]
+		if new_texture == null:
+			speaker_thumbnail_texture.visible = false
+		else:
+			speaker_thumbnail_texture.visible = true
+			speaker_thumbnail_texture.texture = new_texture
+		
 
 
 func display_dialogue(text: String):
--- Scripts/Tools/DialogueInteractionGenerator.gd
+++ Scripts/Tools/DialogueInteractionGenerator.gd
@@ -2,28 +2,15 @@
 class_name DialogueInteractionGenerator
 extends Node
 
-func _get_property_list():
-	return [{
-		"name": "Generate Dialogue Interactions",
-		"type": TYPE_BOOL,
-		"usage": PROPERTY_USAGE_EDITOR
-	}]
-
-func _set(property, value):
-	if property == "Generate Dialogue Interactions" and value:
-		generate()
-		return true
-	return false
-
-func _get(property):
-	return null
+func _ready() -> void:
+	generate()
 
 
 func generate():
 	var parser = YAMLParser.new()
 
 	var yfile = FileAccess.open(
-					"res://story.yml",
+					"res://dialogue_v2.yml",
 					FileAccess.READ)
 	var yaml = yfile.get_as_text()
 	yfile.close()
@@ -35,19 +22,21 @@ func generate():
 		for interaction in result["interactions"]:
 			var dialogue_interaction = DialogueInteractionData.new()
 			var interaction_id = interaction["interaction_id"]
+			print(interaction_id)
 			dialogue_interaction.id = interaction_id
 			for dialogue in interaction.dialogue:
-				dialogue_interaction.dialogue.append(dialogue["text"])
-			for response in interaction.responses:
-				var new_response_data = ResponseData.new()
-				match response["type"]:
-					"goto_dialogue":
-						new_response_data.type = ResponseData.ResponseType.NEW_DIALOGUE
-					_:
-						new_response_data.type = ResponseData.ResponseType.CHANGE_SCENE
-				new_response_data.text = response["text"]
-				new_response_data.payload = response["payload"]
-				dialogue_interaction.responses.append(new_response_data)
+				dialogue_interaction.dialogue.append(DialogueEntry.new(dialogue["speaker"], dialogue["text"]))
+			if interaction.has("responses"):
+				for response in interaction.responses:
+					var new_response_data = ResponseData.new()
+					match response["type"]:
+						"goto_dialogue":
+							new_response_data.type = ResponseData.ResponseType.NEW_DIALOGUE
+						_:
+							new_response_data.type = ResponseData.ResponseType.CHANGE_SCENE
+					new_response_data.text = response["text"]
+					new_response_data.payload = response["payload"]
+					dialogue_interaction.responses.append(new_response_data)
 			ResourceSaver.save(
 				dialogue_interaction,
 				"res://Resources/Generated/DialogueInteraction%d.tres" % i)
